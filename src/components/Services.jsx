66053da54afbe3efe7da1868dce4adb7186887cf
Shivam Soni <shivamsoni8445@gmail.com>
2025-06-12 11:10:31 +0530

    add Service content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,16 @@
 import Bg from "./Bg"
+import Overview from "./Overview"
+import Test from "./Test"
+import GreatWork from "./GreatWork"
 import logo from '/img/bg/img2.png'
-
 
 function Services() {
   return (
     <>
-      <Bg title='Services' imgPath={logo}/>
+      <Bg title='Services' imgPath={logo} />
+      <Overview />
+      <Test />
+      <GreatWork />
     </>
   )
 }

b297926ba78065fe77ce2bd2ad3ae7000b48349f
Shivam Soni <shivamsoni8445@gmail.com>
2025-06-12 11:20:31 +0530

    add Blog content

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,16 @@
 import Bg from "./Bg"
+import Seo from "./Seo"
+import Test from "./Test"
+import GreatWork from "./GreatWork"
 import logo from '/img/bg/img3.png'
 
 function Blog() {
     return (
         <>
             <Bg title='Blog' imgPath={logo}/>
+            <Seo />
+            <Test />
+            <GreatWork />
         </>
     )
 }
